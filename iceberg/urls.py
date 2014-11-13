7a734f336e8916e2bda2707e546967f0986d8f02
Damir Mustafin <frenchthebread@gmail.com>
2014-11-13 16:23:48 +0200

    added missing css file

--- iceberg/urls.py
+++ iceberg/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from app.views import home, cal
+from iceberg import settings
 
 admin.autodiscover()
 
@@ -11,3 +12,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
